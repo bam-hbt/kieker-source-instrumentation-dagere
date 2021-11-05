89f39a255a27aae1aa07ebac264a3ad505607cf0
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-11-05 21:33:36 +0100

    Update tests

--- src/test/java/net/kieker/sourceinstrumentation/it/SimpleProjectUtil.java
+++ src/test/java/net/kieker/sourceinstrumentation/it/SimpleProjectUtil.java
@@ -3,7 +3,9 @@ package net.kieker.sourceinstrumentation.it;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import net.kieker.sourceinstrumentation.AllowedKiekerRecord;
@@ -13,26 +15,37 @@ import net.kieker.sourceinstrumentation.parseUtils.StreamGobbler;
 import net.kieker.sourceinstrumentation.util.TestConstants;
 
 public class SimpleProjectUtil {
-   public static File obtainLogs(final String projectFolder) throws IOException {
+   public static String obtainLogs(final String projectFolder) throws IOException {
       SourceInstrumentationTestUtil.initSimpleProject(projectFolder);
-
-      File tempFolder = new File(TestConstants.CURRENT_FOLDER, "results");
-      if (!tempFolder.mkdir()) {
-         throw new RuntimeException("Could not create directory: " + tempFolder.getAbsolutePath());
-      }
 
       InstrumentKiekerSource instrumenter = new InstrumentKiekerSource(AllowedKiekerRecord.OPERATIONEXECUTION);
       instrumenter.instrumentProject(TestConstants.CURRENT_FOLDER);
 
       final ProcessBuilder pb = new ProcessBuilder("mvn", "test", 
-            "-Djava.io.tmpdir=" + tempFolder.getAbsolutePath());
+            "-Djava.io.tmpdir=" + TestConstants.CURRENT_RESULTS.getAbsolutePath());
       pb.directory(TestConstants.CURRENT_FOLDER);
 
+      String logs = getLogsFromProcessBuilder(pb);
+      return logs;
+   }
+   
+   public static void cleanTempDir() throws IOException {
+      if (TestConstants.CURRENT_RESULTS.exists()) {
+         FileUtils.cleanDirectory(TestConstants.CURRENT_RESULTS);
+      }
+      if (!TestConstants.CURRENT_RESULTS.mkdirs()) {
+         throw new RuntimeException("Could not create " + TestConstants.CURRENT_RESULTS.getAbsolutePath());
+      }
+   }
+   
+   public static String getLogsFromProcessBuilder(final ProcessBuilder pb) throws IOException {
       Process process = pb.start();
       StreamGobbler.showFullProcess(process);
-      
-      File resultFolder = tempFolder.listFiles()[0];
+
+      File resultFolder = TestConstants.CURRENT_RESULTS.listFiles()[0];
       File resultFile = resultFolder.listFiles((FileFilter) new WildcardFileFilter("*.dat"))[0];
-      return resultFile;
+
+      String monitorLogs = FileUtils.readFileToString(resultFile, StandardCharsets.UTF_8);
+      return monitorLogs;
    }
 }
--- src/test/java/net/kieker/sourceinstrumentation/it/SourceInstrumentationEnumIT.java
+++ src/test/java/net/kieker/sourceinstrumentation/it/SourceInstrumentationEnumIT.java
@@ -2,16 +2,11 @@ package net.kieker.sourceinstrumentation.it;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import net.kieker.sourceinstrumentation.AllowedKiekerRecord;
@@ -19,15 +14,9 @@ import net.kieker.sourceinstrumentation.InstrumentationConfiguration;
 import net.kieker.sourceinstrumentation.JavaVersionUtil;
 import net.kieker.sourceinstrumentation.SourceInstrumentationTestUtil;
 import net.kieker.sourceinstrumentation.instrument.InstrumentKiekerSource;
-import net.kieker.sourceinstrumentation.parseUtils.StreamGobbler;
 import net.kieker.sourceinstrumentation.util.TestConstants;
 
 public class SourceInstrumentationEnumIT {
-
-   @BeforeEach
-   public void before() throws IOException {
-      FileUtils.deleteDirectory(TestConstants.CURRENT_FOLDER);
-   }
 
    public File writeAdaptiveInstrumentationInfo() throws IOException {
       final File configFolder = new File(TestConstants.CURRENT_FOLDER, "config");
@@ -46,25 +35,17 @@ public class SourceInstrumentationEnumIT {
    public void testExecution() throws IOException {
       SourceInstrumentationTestUtil.initSimpleProject("/example_enum/");
 
-      File tempFolder = new File(TestConstants.CURRENT_FOLDER, "results");
-      tempFolder.mkdir();
-
       InstrumentationConfiguration configuration = new InstrumentationConfiguration(AllowedKiekerRecord.DURATION, true, null, false, false, 10, false);
       
       InstrumentKiekerSource instrumenter = new InstrumentKiekerSource(configuration);
       instrumenter.instrumentProject(TestConstants.CURRENT_FOLDER);
 
       final ProcessBuilder pb = new ProcessBuilder("mvn", "test",
-            "-Djava.io.tmpdir=" + tempFolder.getAbsolutePath());
+            "-Djava.io.tmpdir=" + TestConstants.CURRENT_RESULTS.getAbsolutePath());
       pb.directory(TestConstants.CURRENT_FOLDER);
 
-      Process process = pb.start();
-      StreamGobbler.showFullProcess(process);
-
-      File resultFolder = tempFolder.listFiles()[0];
-      File resultFile = resultFolder.listFiles((FileFilter) new WildcardFileFilter("*.dat"))[0];
-
-      String monitorLogs = FileUtils.readFileToString(resultFile, StandardCharsets.UTF_8);
+      String monitorLogs = SimpleProjectUtil.getLogsFromProcessBuilder(pb);
+      
       MatcherAssert.assertThat(monitorLogs, Matchers.containsString("public void de.peass.MainTest.testMe()"));
       if (JavaVersionUtil.getSystemJavaVersion() > 8) {
          MatcherAssert.assertThat(monitorLogs, Matchers.not(Matchers.containsString("public void de.peass.AddRandomNumbers.addSomething();")));
