742cd9f018686c71b8fc1393a1e97bef9ef55be0
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-11-22 21:00:02 +0100

    Fix if statement handling

--- kieker-source-instrumentation-library/src/main/java/net/kieker/sourceinstrumentation/instrument/ReachabilityDecider.java
+++ kieker-source-instrumentation-library/src/main/java/net/kieker/sourceinstrumentation/instrument/ReachabilityDecider.java
@@ -7,6 +7,7 @@ import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.CatchClause;
 import com.github.javaparser.ast.stmt.DoStmt;
 import com.github.javaparser.ast.stmt.ForStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.ReturnStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.stmt.SwitchEntry;
@@ -59,11 +60,24 @@ public class ReachabilityDecider {
       } else if (last instanceof ForStmt) {
          ForStmt stmt = (ForStmt) last;
          Optional<Expression> end = stmt.getCompare();
-         
+
          // Do not change to isEmpty until only Java 11 is used
          if (!end.isPresent()) {
             return true;
          }
+      } else if (last instanceof IfStmt) {
+         IfStmt ifStatement = (IfStmt) last;
+         if (ifStatement.getElseStmt().isPresent()) {
+            Statement elseStatement = ifStatement.getElseStmt().get();
+            boolean elseAfterEndIsUnreachable = isAfterLastUnreachable(elseStatement);
+            if (elseAfterEndIsUnreachable) {
+               boolean thenAfterEndIsUnreachable = isAfterLastUnreachable(ifStatement.getThenStmt());
+               if (elseAfterEndIsUnreachable && thenAfterEndIsUnreachable) {
+                  return true;
+               }
+            }
+         }
+         // boolean before
       } else if (last instanceof SwitchStmt) {
          SwitchStmt switchStmt = (SwitchStmt) last;
          Optional<SwitchEntry> optionalLastSwitch = switchStmt.getEntries().getLast();
